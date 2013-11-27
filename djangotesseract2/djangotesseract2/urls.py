027df53ebc17fd218e683a16a0417b8c04d45d80
joash xu <joash.xu@gmail.com>
2013-11-27 12:26:20 +0700

    Remove faulty URL line.

--- djangotesseract2/djangotesseract2/urls.py
+++ djangotesseract2/djangotesseract2/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^oauth2/', include('provider.oauth2.urls', namespace='oauth2')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('documentextractor.urls')),
 )
