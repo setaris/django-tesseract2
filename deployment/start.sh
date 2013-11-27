3d12ebce10fb1da3dc007c53bba00a79d8f2a27e
joash xu <joash.xu@gmail.com>
2013-11-27 14:07:57 +0700

    Service naming issue fix.

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-LOGFILE=/home/ubuntu/webapps/setarisocr/setarisocr.log
+LOGFILE=/home/ubuntu/webapps/djangotesseract2/djangotesseract2.log
 NUM_WORKERS=2
 USER=ubuntu
 
 # user/group to run as
-cd /home/ubuntu/webapps/setarisocr/setarisocr
-source /home/ubuntu/envs/setarisocrenv/bin/activate
-exec python manage.py run_gunicorn --bind=127.0.0.1:8000 --settings=setarisocr.settings.production --workers=$NUM_WORKERS --timeout=120 --graceful-timeout=120 1>>$LOGFILE 2>>$LOGFILE
+cd /home/ubuntu/webapps/djangotesseract2/djangotesseract2
+source /home/ubuntu/envs/djangotesseract2env/bin/activate
+exec python manage.py run_gunicorn --bind=127.0.0.1:8000 --settings=djangotesseract2.settings.production --workers=$NUM_WORKERS --timeout=120 --graceful-timeout=120 1>>$LOGFILE 2>>$LOGFILE
